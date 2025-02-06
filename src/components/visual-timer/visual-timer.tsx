8d309c73be5be58bc60217dc4e3782c9332a4592
Marine Lancien (Hexagone IT) <marine.lancien@hexagone-it.fr>
2025-02-06 16:43:47 +0100

    add marker

--- src/components/visual-timer/visual-timer.tsx
+++ src/components/visual-timer/visual-timer.tsx
@@ -28,12 +28,12 @@ export default function VisualTimer({ currentTime = 25*60 }: VisualTimerProps) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         const [centerX, centerY] = [canvas.width / 2, canvas.height / 2];
-        const radius = canvas.width / 2 - 20;
+        const radius = canvas.width / 2 - 30;
+        const strokeWidth = 5;
 
         // Create angle
         const startAngle = - Math.PI / 2;
         const angle = (currentTime / MAX_TIMER) * (2 * Math.PI) // 360 degrés;
-
 
         ctx.beginPath();
         ctx.moveTo(centerX, centerY);
@@ -42,15 +42,44 @@ export default function VisualTimer({ currentTime = 25*60 }: VisualTimerProps) {
         ctx.fillStyle = colors.amber['500'];
         ctx.fill();
 
-
         // Create circle
         ctx.beginPath();
-        ctx.moveTo(centerX + radius, centerY);
+        ctx.moveTo(centerX, centerY);
         ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, true);
-        ctx.arc(centerX, centerY, radius - 5, 0, 2 * Math.PI, true);
+        ctx.arc(centerX, centerY, radius - strokeWidth, 0, 2 * Math.PI, true);
         ctx.fillStyle = colors.amber['600'];
         ctx.closePath();
         ctx.fill("evenodd");
+
+        // add markers
+        for ( let i = 0; i < 60; i += 1 ) {
+
+            const iAngle = (i * 60 / MAX_TIMER) * (2 * Math.PI) + startAngle;
+            const length = i % 5 === 0 ? 4 : 2;
+            const width = i % 5 === 0 ? 4 : 2;
+
+
+            ctx.beginPath();
+            ctx.moveTo(centerX + Math.cos(iAngle) * (radius - strokeWidth - length), centerY + Math.sin(iAngle) * (radius - strokeWidth - length));
+            ctx.lineTo(centerX + Math.cos(iAngle) * (radius + length), centerY + Math.sin(iAngle) * (radius + length));
+            ctx.strokeStyle = 'black';
+            ctx.lineWidth = width;
+            ctx.stroke();
+            ctx.closePath();
+
+            if (i % 5 === 0) {
+
+                const textRadius = radius + 15; // Position des chiffres un peu à l'extérieur du cercle
+                const textX = centerX + Math.cos(iAngle) * textRadius;
+                const textY = centerY + Math.sin(iAngle) * textRadius;
+
+                ctx.font = "12px Arial";
+                ctx.fillStyle = "black";
+                ctx.textAlign = "center";
+                ctx.textBaseline = "middle";
+                ctx.fillText(i.toString(), textX, textY);
+            }
+        }
 
     }, [currentTime]);
 
